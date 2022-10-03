1df82ea3b05df068791e9a8e8e983c914c392b47
cursedcursess <114909917+cursedcursess@users.noreply.github.com>
2022-10-03 11:34:39 +0300

    Create pe-pe.sol

---
+++ pe-pe.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.6.12;
+
+contract Manager {
+    function performTasks() public {}
+
+    function configCode() public pure returns (address) {
+        return 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    }
+}
